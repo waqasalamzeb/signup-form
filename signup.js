32eca2c4bf10e6b6c3d5e7d3788e5f86051cb96c
waqasalamzeb <79015762+waqasalamzeb@users.noreply.github.com>
2023-06-24 01:34:59 -0700

    done

--- signup.js
+++ signup.js
@@ -1,14 +1,22 @@
+
+
 function Submit(){
-
-
-
+   
 // const username = document.getElementById("username").value
+if(email.value==""&& password.value==""){
+    Swal.fire(
+        "The Fields can't be empty"
+        
+      )
+}
+else{
+    
 const email = document.getElementById("email").value
 const password = document.getElementById("password").value
+
 const allUsers = JSON.parse(localStorage.getItem("users"))
+
 let foundUser = false;
-
-
 
 if (!allUsers) {
     const users = [];
@@ -17,23 +25,43 @@ if (!allUsers) {
 } else {
     //use find loop for better approach
     for (i = 0; i < allUsers.length; i++) {
-        if (allUsers[i].email == email && password==password) {
+        if (allUsers[i].email == email && allUsers[i].password==password) {
             foundUser = true;
-            window.location.href="https://waqasalamzeb.github.io/loginform/"
+           
+           
         }
     }
+}
 
     if (foundUser) {
         Swal.fire({
             icon: 'error',
             title: 'Credentials Error',
             text: 'Email Already Exist!',
+            
         })
+       
     } else {
         allUsers.push({  email, password })
+       
         localStorage.setItem("users", JSON.stringify(allUsers))
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Login Successfully',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          window.location.href="https://waqasalamzeb.github.io/loginform/"
+
+
+      
+        
+       
         console.log("data saved")
+       
     }
 }
 }
+
 
